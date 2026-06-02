b325bd9c486584359b9ac8c640caf6cab31394f1
fcaretti <federico.caretti@protonmail.com>
2026-06-02 11:42:53 +0200

    Small fix to catch error with the old name

--- src/topomics/__init__.py
+++ src/topomics/__init__.py
@@ -1,8 +1,15 @@
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
 from . import models, pl, pp, tl
 from .models import BaseTopicModel, MultimodalAmortizedLDA, ShareTopic_LDA_Multi, SVEM_LDA_Multi
 
 __all__ = ["pl", "pp", "tl", "models", "SVEM_LDA_Multi", "BaseTopicModel", "MultimodalAmortizedLDA", "ShareTopic_LDA_Multi"]
 
-__version__ = version("topomics")
+# Package was renamed from "omics-topic" -> "topomics"; tolerate stale editable installs.
+try:
+    __version__ = version("topomics")
+except PackageNotFoundError:
+    try:
+        __version__ = version("omics-topic")
+    except PackageNotFoundError:
+        __version__ = "0.0.0+unknown"
